13033412cf4bc4984f4de7d92dc253158d915c9b
Nabin bhandari <bnabin555@gmail.com>
2024-05-11 11:37:32 +0545

    Deployment changes\!

--- backend/utils/envData.js
+++ backend/utils/envData.js
@@ -1,7 +1,8 @@
 
 const { PORT, MONGODB_URI, JWT_ACCESS_TOKEN, JWT_REFRESH_TOKEN  ,
-    REACT_APP_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
+     GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 } = process.env;
+const REACT_APP_URL = 'http://localhost:5173';
 
 module.exports = {
     PORT,
